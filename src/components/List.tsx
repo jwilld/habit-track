851d880116e61bec4adfd00a4263d5d6c57524de
Jerry Davidson <jerrydavidson@jerrys-mbp-2.mynetworksettings.com>
2022-01-16 15:14:41 -0500

    add props to List component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,7 @@
-export default function List() {
-  return <ul className="list-group list-group-flush"></ul>;
+interface ListProps {
+  items: string[];
+}
+export default function List(props: ListProps) {
+  const listItems = props.items.map((item: string, i: number) => <li key={i}>{item}</li>);
+  return <ul className="list-group list-group-flush">{listItems}</ul>;
 }
