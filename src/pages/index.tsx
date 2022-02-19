2a25beb35d68e4715679d9f7d5d01f79ac6e2ca0
Jerry Davidson <jerrydavidson@jerrys-mbp-2.mynetworksettings.com>
2022-02-19 15:09:54 -0500

    create component for log modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,27 @@ import SearchInput from '../components/SearchInput';
 import { GlobalStateProvider } from '../context/state';
 
 const Home: NextPage = () => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const years = [2021, 2022, 2023, 2024, 2025].map((year: number, i: number) => {
+    return <option key={i}>{year}</option>;
+  });
+  const monthOptions = months.map((month: string, i: number) => {
+    return <option key={i}>{month}</option>;
+  });
+
   return (
     <GlobalStateProvider>
       <div className={styles.container}>
@@ -19,7 +40,22 @@ const Home: NextPage = () => {
         </Head>
         <main className={styles.main}>
           <h1 className={styles.app_title}> Habit Track</h1>
-          <BarChart/>
+          {/* <BarChart /> */}
+          <div style={{ display: 'flex' }}>
+            <select className="form-select" aria-label="Default select example">
+              {monthOptions}
+            </select>
+            <input
+              style={{ width: '300px' }}
+              type="date"
+              id="start"
+              name="trip-start"
+              value="2018-07-22"
+              min="2018-01-01"
+              max="2018-12-31"
+            ></input>
+          </div>
+
           <div style={{ overflowY: 'scroll', height: '400px' }}>
             <Card />
           </div>
