c867f42a78c4c8e941369ef487eafc0275962a4b
Jerry Davidson <jerrydavidson@jerrys-mbp-2.mynetworksettings.com>
2022-02-19 15:45:59 -0500

    track LogModal

---
+++ src/components/LogModal.tsx
@@ -0,0 +1,37 @@
+import { Modal } from 'react-bootstrap';
+import List from './List';
+import { LogTimestamp, LogObject } from '../interfaces/interfaces';
+import BarChart, { getHourMinuteSecond, getMonthDayYear, dateToUSEST } from './BarChart';
+
+interface LogModalProps {
+  show: boolean;
+  close: Function;
+  title: string;
+  timestamps: LogTimestamp[];
+}
+
+export default function LogModal(props: LogModalProps) {
+  return (
+    <>
+      <Modal
+        show={props.show}
+        fullscreen="xxl-down"
+        onHide={() => {
+          props.close();
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <BarChart />
+          <List
+            items={props.timestamps.map((timestamp) =>
+              dateToUSEST(`${getMonthDayYear(timestamp)} ${getHourMinuteSecond(timestamp)}`)
+            )}
+          />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
